5ff8b1c8b61453552d380bd885f726f394ef3d4e
Brian Kang <bhkangw@gmail.com>
2017-11-18 20:00:00 -0800

    Updated Algorithms resource

---
+++ algoPractice.js
@@ -0,0 +1,669 @@
+// October 18
+
+function PopFront(arr){ //pop the first value of the array out of the array
+	var temp = arr[0];
+	for(var i = 0; i < arr.length; i++){
+		arr[i] = arr[i+1]
+	}
+	pop.arr();
+	return temp
+	return arr
+}
+
+function PushFront(arr,val){ //push the last value of the array to the front
+	var temp = arr[arr.length-1];
+	for(var i = arr.length-1; i < 0; i--){
+		arr[i] = arr[i-1];
+	}
+	arr[arr.length-1] = temp;
+	return arr
+}
+
+function RemoveAt(){ //remove a value from specified position
+
+}
+
+
+// October 19
+
+function Reverse(arr){ //reverse the array
+	for(var i = 0; i < arr.length/2; i++){
+		var temp = arr[i];
+		arr[i] = arr[arr.length-1-i];
+		arr.length-1-i = temp;
+	}
+	return	arr
+}
+
+function RemoveDupes(arr){ //remove duplicates from array
+	// (less efficient)
+	var newarr = []
+	for(var i = 0; i < arr.length; i++){
+		if(arr[i] !== arr[i+1]){
+			newarr.push(arr[i])
+		}
+	}
+	return newarr;
+}
+
+function RemoveDupes(arr){ //remove duplicates from array
+	// (more efficient)
+}
+
+// October 23
+
+function Remove(str){ //remove all spaces from string
+	var newstr;
+	for(var i = 0; i < str.length; i++){
+		if(str[i] !== " "){
+			newstr = str[i];
+		}
+	}
+	return newstr;
+}
+
+function Acronym(str){ //create acronym from string - new string with only first letter of each word
+	var newstr = str[0];
+	for(var i = 0; i < str.length; i++){
+		if(str[i] === " "){
+			newstr = newstr + str[i+1];
+		}
+	}
+	return newstr;
+}
+
+// October 24
+
+function PrintKeys(obj){ //print all the keys of an object
+	for(k in obj){
+		console.log(k);
+	}
+}
+
+function PrintVals(obj){ //print all the values of an object
+	for(k in obj){
+		console.log(obj[k]);
+	}
+}
+
+function Zip(arr1,arr2){ // v1 combine two arrays into a single object - arrays are both the same length
+	for(var i = 0; i < arr1.length; i++){
+		Zip(k) = arr1[i];
+	}
+	for(var i = 0; i < arr2.length; i++){
+		Zip(obj(k)) = arr2[i];
+	}
+	return Zip;
+}
+
+function Zip(arr1,arr2){ // v2 more efficient
+	var obj = {}
+	for(var i = 0; i < arr1.length; i++){
+		obj(arr1[i]) = arr2[i];
+	}
+
+
+function UnZip(obj){ // v1 given an object with two arrays, print both arrays separately
+	for(k in obj){
+		arr1() = k;
+	}
+	for(k in obj){
+		arr2() = obj(k);
+	}
+	console.log(arr1);
+	console.log(arr2);
+}
+
+function UnZip(obj){ // v2 given an object with two arrays, print both arrays separately - USING PUSH
+	for(k in obj){
+		arr1.push(k);
+	}
+	for(k in obj){
+		arr2.push(obj[k]);
+	}
+	console.log(arr1);
+	console.log(arr2);
+}
+
+
+// October 25
+
+function IsPalindrome(str){ // check if a string is a palindrome ie. racecar, mom, noon
+	var palindrome;
+	for(var i = 0 ; i < str.length/2; i++){
+		if(str[i] = str[str.length-1-i]){
+			palindrome = true;
+		}
+		else{palindrome = false
+			break;
+		}
+	}
+	console.log(palindrome);
+	return palindrome;
+}
+
+function IsPangram(str){ // check if a string is a pangram (contains all letters of the alphabet)
+	var pangram
+	str = str.toLowerCase();
+	for(var i = 97; i < 123; i++){
+		for(var j = 0; i < str.length; i++){
+			if(i === str[j]){
+				pangram = true;
+			}
+			else{pangram = false;
+				break;
+			}
+		}
+	}
+	console.log(pangram);
+	return pangram;
+}
+
+// October 26
+
+function CoinChange(num){ // give change with least amount of coins ie. given change of 81 cents, return 3 quarters, 1 nickel & 1 penny.
+	var obj = {}
+	obj["quarters"] = Math.floor(num%25);
+	num -= Math.floor(num%25)*25;
+	obj["dimes"] = Math.floor(num%10);
+	num -= Math.floor(num%10)*10;
+	obj["nickels"] = Math.floor(num%5);
+	num -= Math.floor(num%5)*5;
+	obj["pennies"] = num;
+	return obj;
+}
+
+function NumToOne(num){ // return the smallest integer ie. 4712 -> 14 -> 5 (??)
+
+}
+
+// October 27
+
+function DoubleTrouble(arr){ // return an array with duplicated values together ie. [4,10,7] -> [4,4,10,10,7,7]
+	arr = arr*2
+	for(var i = arr.length/2-1; i > 0; i--){
+		arr[i*2] = arr[i];
+		arr[i*2+1] = arr[i];
+	}
+	return arr
+}
+
+function DoubleTrouble(arr){ // different way to return an array with duplicated values together ie. [4,10,7] -> [4,4,10,10,7,7]. Haven't tested
+	for(var i = 0; i < arr.length; i++){
+		arr.push(i);
+	}
+	for(var i = arr.length/2; i < arr.length; i+=2){
+		var temp = arr[i];
+		arr[i] = arr[i - arr.length/2 - 1];
+		arr[arr.length/2 - 1] = temp;
+	}
+}
+
+
+// October 30
+
+function ReverseString(str){ // reverse a string ie. "Hello" -> "olleH"
+	newstr = ""
+	for(var i = str.length-1; i >= 0; i--){
+		newstr = newstr + str[i];
+	}
+	return newstr
+}
+
+function NumToRomanNumeral(num){ // write the roman numeral equivalent of a number ie. 349 -> CCCXLIX
+	roman = [1000,500,100,50,10,5,1]
+	str = ""
+	for(var i = 0; i < arr.length; i++){
+		if(num > arr[i])
+			str = str + math.Floor(num % arr[i])
+
+	}
+}
+
+// October 31
+
+function BracesValid(str){ // check if brackets are correct. Hap(ppy[H])all[owee]{n}! = correct; Hap(p[)y] = incorrect
+	var stack = [];
+	for(var i = 0; i < str.length; i++){
+		if(str[i] = "["){
+			stack.push("[");
+		}
+		else if(str[i] = "("){
+			stack.push("(");
+		}
+		else if(str[i] = "{"){
+			stack.push("{");
+		}
+		else if(str[i] = "]"){
+			if(stack[stack.length-1] === "["){
+				stack.pop("[");
+			}
+			else{
+				break;
+			}
+		}
+		else if(str[i] = ")"){
+			if(stack[stack.length-1] === "("){
+				stack.pop("(");
+			}
+			else{
+				break;
+			}
+		}
+		else if(str[i] = "}"){
+			if(stack[stack.length-1] === "{"){
+				stack.pop("{");
+			}
+			else{
+				break;
+			}
+		}
+	}
+	if(stack === []){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+// November 1
+
+function LongestPalindrome(str){ // identify the longest palindrome. If there are multiple of same length, return the latest.
+	var max_pal = ""				// ex. "Aha noon racecar" = racecar || "AABBCCDD" = DD
+	for(var i = 0; i < str.length; i++){
+		for(var j = i; i < str.length; j++){
+			var str_cur = str.slice(i,j);
+			if(IsPalindrome(str_cur)){
+				if(max_pal.length <= str_cur.length){
+					max_pal = str_cur;
+				}
+			}
+		}
+	}
+	return max_pal
+}
+
+
+// November 2
+
+function IsSuperVocalic(str){ // return if the word has each vowel (and ONLY one of each?)
+	var vow = [a,e,i,o,u];
+	for(var i = vow[vow.length-1]; i >=0; i++){
+		for(var j = 0; j < str.length; j++){
+			if(vow[i] == str[j]){
+				vow.pop()
+			}
+			else{
+				break
+			}
+		}
+	}
+	if vow = []{
+		return true
+	}
+	else{
+		return false
+	}
+}
+
+function IsSuperVocalic(str){ // above still returns true if it has multiple vowels. Below does not:
+	var vow = {
+		"a": 0,
+		"e": 0,
+		"i": 0,
+		"o": 0,
+		"u": 0;
+	}
+	for(var k in vow){
+		for(var j = 0; j < str.length; j++){
+			if(k == str[j]){
+				p(k) = 1
+			}
+		}
+	}
+	if(vow = {
+		"a": 1,
+		"e": 1,
+		"i": 1,
+		"o": 1,
+		"u": 1;
+	}){
+		return true
+	}
+	else{
+		return false
+	}
+}
+
+function CommonSuffix(array){ // checks if the all strings in the array share a common suffix ie. Baby Lazy Play Hazy = 'zy' = false. Fit Cat = 't' = true.
+
+}
+
+// November 3 
+
+// Introducing linked list!
+
+function SLNode(v){ // function to create a new node class
+	this.val = v
+	this.next = null;
+}
+
+function SLList(){ // function to create a new list class
+	this.head = null;
+	this.AddToFront = function(num){ // function to [Add a New Node to the Front] <-- Today's algorithm question
+		var newNode = SLNode(num)
+		if(this.head == null){ // if statement not necessary at all. only helpful for own visualization of logic.
+			this.head = newNode;
+			return this;
+		}
+		newNode.next = this.head; // this statement is key: always sets the new node to whatever the head was pointing to
+		this.head = newNode; // and then sets the head to point to the new node
+		return this
+	}
+}
+
+// November 6
+
+function SLList(val){
+	this.head = null;
+	this.contains = function(v){ // first function: find if the link list CONTAINS value v
+		var runner = this.head;
+		if(this.head == null){
+			return false;
+		}
+		while(runner.next){ // aka while(runner.next !== null)
+			if(runner.val == v){
+				return true;
+			}
+			runner = runner.next;
+		}
+		return false;
+	}
+	this.max = function(){ // second function: find the MAX value in a link list
+		var runner = this.head;
+		var max = runner.val;
+		if(this.head == null){
+			return null;
+		}
+		while(runner.next){
+			if(max < runner.next.val){
+				max = runner.next.val;
+			}
+			runner = runner.next
+		}
+		return max;
+	}
+	this.size = function(){ // third function: return the SIZE of the link list
+		var runner = this.head;
+		var size = 0;
+		if(this.head == null){
+			return size
+		}
+		while(runner.next){
+			count++
+		}
+		return count;
+	}
+	this.size = function(){ // third function: alternate method, checking if runner == null instead of runner.next
+		var runner = this.head;
+		var size = 0;
+		while(runner !== null){
+			count++
+			runner = runner.next;
+		}
+		return count;
+	}
+}
+
+// November 7
+
+function SLNode(v){ // need to create the function for when we call on it in the AddToBack function
+	this.val = v;
+	this.next = null;
+}
+
+function SLList(){ // creating the new list class
+	this.head = null;
+	this.AddToBack = function(v){ // add a node to the back of the list
+		var runner = this.head;
+		while(runner.next ! == null){
+			runner = runner.next
+		}
+		runner.next = new SLNode(v); // creating a new node by making an instance of the class
+	}
+
+	this.RemoveBack = function(){ // remove a node from the back of the list
+		var runner = this.head;
+		while(runner){
+			runner = runner.next // don't forget this! in order to iterate through the list
+			if(runner.next.next == null){ // checking two nodes ahead
+				runner.next = null;
+			}
+			if(this.head.next == null){ // corner case when there is only one node
+				this.head = null;
+			}
+		}
+	}
+
+	this.RemoveFront = function(){ // remove the first node from the list
+		this.head = this.head.next;
+	}
+}
+
+// Novemeber 8
+
+function MaxToBack(){ // find the node with the max value and move that node to the back of the list
+	var runner = this.head;
+	var max = this.head;
+	var beforemax;
+	while (runner.next! = null){
+		if(runner.next.val > max.val){
+			beforemax = runner; // must grab BOTH the node of the max as well as the node before (for the pointer)
+			max = runner.next; // sets the variable max as the node with the max value
+		}
+		runner = runner.next;
+	}
+	if(runner.next == null){
+		runner.next = max;
+		beforemax.next = max.next; // must set this pointer before setting max.net to null
+		max.next = null;
+	}
+	if(this.head = max){ // edge case in case the max value is the first node
+		this.head = this.head.next;
+	}
+}
+
+function InsertBefore(val, before){ // create a node with value val and insert it before the specified node
+
+}
+
+// November 9
+
+function SLNode(v){ // function to create a node for InsertAfter
+	this.val = v;
+	this.next = null;
+}
+
+function InsertAfter(val, after){ // create a node with value val and insert it AFTER the specified node
+	var runner = this.head;
+	var newnode = SLNode(after)
+	var found;
+	while(runner.next != null){
+		if(runner.val == after){
+			found = runner
+		}
+		runner = runner.next
+	}
+	newnode.next = temp.next
+	temp.next = newnode
+}
+
+function RemoveVal(val){ // remove any instance of a node with the value of val from the list
+	var runner = this.head
+	if(this.head.val == val){
+		this.head = this.head.next
+	}
+	while(runner.next.next != null){
+		if(runner.next.val == val){
+			runner.next = runner.next.next
+		}
+	runner = runner.next
+	}
+}
+
+// November 13 
+
+// Introducing Queues!
+
+function SLNode(v){ // function to create a node for Enqueue
+	this.val = v;
+	this.next = null;
+}
+
+function SLQueue(){ // create a new node and add it to the back of the queue (AddToBack)
+	this.head = null
+	this.Enqueue = function(v){
+		var newnode = SLNode(v);
+		if(this.head == null){
+			this.head = newnode;
+		}
+		else{
+			var runner = this.head;
+			while(runner.next){
+				runner = runner.next;
+			}
+			runner.next = newnode;
+		}
+	}
+	this.Dequeue = function(){ // remove the first node of the list and return its value (PopFront)
+		var node = this.head;
+		if(this.head == null){
+
+		}
+		else{
+			this.head = this.head.next;
+			return node;
+		}
+	}
+}
+
+// November 14
+
+// Introducing Stacks!
+
+function arrstack(){ // add and remove value from an array stack
+	function push(val){
+		arr[arr.length] = val
+	}
+	function pop(){
+		temp = arr[arr.length-1]
+		arr.length = arr.length-1 // you can just set the array to a new length to 'trim' out the rest!
+		return temp
+	}
+}
+
+function SLNode(v){ // function to create a node for below function
+	this.val = v;
+	this.next = null;
+}
+
+function linkedliststack(){ // add and remove from a linked list stack
+	function push(val){
+		var newnode = SLNode(val);
+		newnode.next = this.head;
+		this.head = newnode;
+	}
+	function pop(){
+		temp = this.head;
+		this.head = this.head.next
+		return temp
+	}
+}
+
+function size(){ // return the size of an array stack
+	var size = arr.length
+	return size
+}
+function size(){ //  return the size of a linked list stack
+	var runner = this.head
+	var count = 0
+	while(runner != null){
+		count += 1
+		runner = runner.next
+	}
+	return count
+}
+
+// November 15
+
+function CQueue(c){ // Introducing circular queue
+	this.data = []
+	this.capacity = c
+	var front = 0
+	var back = i
+
+	this.Enqueue = function(v){ // add a value to the queue - if possible return true, if not return false
+		if(this.data.length === this.capacity){
+			return false
+		}
+		for(var i = 0; i < c; i++){
+			if(!data[i]){
+			data[i] = v
+			return true
+			}
+		}
+	}
+
+	this.Dequeue = function(){ // remove and return the value at the front of the line, have the front line position move appropriately
+		val = this.data[front]
+		this.data[front]=null
+		if(this.data[front+1] == c){
+			if(this.data[0]){
+				this.front=this.front[0]
+			}
+		}
+		if(this.data[front+1]){
+			this.front=this.front+1
+		}
+		return val
+	}
+}
+
+// November 17
+
+function QueueFromStacks(){ // add and remove from a queue that uses stacks exclusively
+	this.stack1 = new Stack()
+	this.stack2 = new Stack()
+	this.enqueue = function(v){
+		this.stack1.add(v)
+	}
+	this.dequeue = function(){
+		size = this.stack1.size()
+		for(var i = 0; i < size; i++){
+			this.stack2.add(this.stack1.remove())
+		}
+		return this.stack2.remove()
+	}
+}
+
+function Stack(){ // only written for use by above function 
+	this.data = []
+	this.add - function(v){
+		this.data[this.data.length] = v
+	}
+	this.remove = function(v){
+		var last = this.data[this.data.length-1]
+		this.data.length = this.data.length-1
+		return last
+	}
+	this.size = function(){
+		return this.data.length
+	}
+}
+
+
+
+
+
+
