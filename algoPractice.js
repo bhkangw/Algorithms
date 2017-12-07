d4769a5c8289772dd07b6d45c3c076df728a158a
Brian Kang <bhkangw@gmail.com>
2017-12-06 23:25:33 -0800

    Added recursiveFibonacci

--- algoPractice.js
+++ algoPractice.js
@@ -772,6 +772,18 @@ function rFactorial(n){
 	return n*rFactorial(n-1);
 }
 
+// December 5 
+
+function rFibonacci(n){
+	if(n == 2){
+		return 1;
+	}
+	if(n == 1){
+		return 0;
+	}
+	return rFibonacci(n-1) + rFibonacci(n-2)
+}
+
 
 
 
